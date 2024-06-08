9eeac73c925aac4dfadc95765e3cab7b97cb9d55
Kanniappan <suriyastart007@gmail.com>
2024-06-08 23:16:51 +0530

    atlas db connected

--- db.js
+++ db.js
@@ -4,9 +4,9 @@ let database;
 
 async function getDatabase() {    
     // mongoose.connect('mongodb://localhost:27017/library')
-    mongoose.connect('mongodb://localhost:27017/shop')
+    mongoose.connect('mongodb+srv://suriyastart007:v3ERWDKwFukUUWqZ@cluster0.0nefcyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     .then(()=>console.log('connected to database'))
-    .catch(()=> console.log('Not connected to database'))
+    .catch((err)=> console.log('Not connected to database',err))
 }
 
 
