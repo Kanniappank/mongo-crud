f18b1adb02581f15471af4ba984dc159405055d1
Kanniappan <suriyastart007@gmail.com>
2024-06-08 23:22:32 +0530

    atlas removed

--- db.js
+++ db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 let database;
 
 async function getDatabase() {    
-    // mongoose.connect('mongodb://localhost:27017/library')
-    mongoose.connect('mongodb+srv://suriyastart007:v3ERWDKwFukUUWqZ@cluster0.0nefcyt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    mongoose.connect('mongodb://localhost:27017/library')
     .then(()=>console.log('connected to database'))
     .catch((err)=> console.log('Not connected to database',err))
 }
